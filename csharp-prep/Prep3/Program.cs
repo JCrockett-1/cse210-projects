c73a5cca2ab239ad5ac2426679ee911b5cf8e219
JCrockett-1 <cro21013@byui.edu>
2025-01-13 13:42:44 -0700

    Completed prep 3 assignment with the 3 core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+
+        bool hasGuessed = false;
+        do
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+                hasGuessed = false;
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+                hasGuessed = false;
+            }
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                hasGuessed = true;
+            }
+        } while (hasGuessed == false);
+
+
+
+
     }
 }
