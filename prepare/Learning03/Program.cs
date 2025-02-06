62b8b1fb964556691ea82a427858f417ba160303
JCrockett-1 <cro21013@byui.edu>
2025-02-06 15:57:44 -0700

    Completed Learning03 Assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction FractionOne = new Fraction();
+        
+        Console.WriteLine(FractionOne.GetFractionString());
+        Console.WriteLine(FractionOne.GetDecimalValue());
+        
+        Fraction FractionTwo = new Fraction(5);
+        
+        Console.WriteLine(FractionTwo.GetFractionString());
+        Console.WriteLine(FractionTwo.GetDecimalValue());
+        
+        Fraction FractionThree = new Fraction(3,4);
+        
+        Console.WriteLine(FractionThree.GetFractionString());
+        Console.WriteLine(FractionThree.GetDecimalValue());
+        
+        Fraction FractionFour = new Fraction(1,3);
+        
+        Console.WriteLine(FractionFour.GetFractionString());
+        Console.WriteLine(FractionFour.GetDecimalValue());
     }
 }
