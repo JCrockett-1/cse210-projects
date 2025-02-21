b95c160a90eab1e58a6ae4d858e4bd8c1a7ee26c
JCrockett-1 <cro21013@byui.edu>
2025-02-20 17:05:44 -0700

    finished Learning Activity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment homework = new Assignment("James Crockett", "Math");
+        Console.WriteLine(homework.GetSummary());
+
+        MathAssignment mathHomework = new MathAssignment("Santiago Crockett", "Division", "2.9", "20-21");
+        Console.WriteLine(mathHomework.GetSummary());
+        Console.WriteLine(mathHomework.getHomeworkList());
+
+
+        WritingAssignment writingHomework = new WritingAssignment("Mary Crockett", "American History", "The Lone Star State");
+        Console.WriteLine(writingHomework.GetSummary());
+        Console.WriteLine(writingHomework.getWritingInformation());
     }
 }
