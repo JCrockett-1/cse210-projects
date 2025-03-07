6e3257f431e3791c8c6f3dad38685e41d5fdebea
JCrockett-1 <cro21013@byui.edu>
2025-03-06 17:01:04 -0700

    Finished Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s = new Square("red", 4);
+        Rectangle r = new Rectangle("green", 4, 2);
+        Circle c = new Circle("blue", 2);
+
+        List<Shape> Shapes = new List<Shape>();
+        Shapes.Add(s);
+        Shapes.Add(r);
+        Shapes.Add(c);
+
+        foreach(Shape shape in Shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
